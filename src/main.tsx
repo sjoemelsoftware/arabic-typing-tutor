8a4c2d6fb22e0a1791e4ff7590279039c7f4c2bc
Joep de Jong <joep@joepdejong.com>
2025-03-07 13:40:51 +0100

    Change toast theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer />
+    <ToastContainer theme="dark" />
     <App />
   </StrictMode>
 );
